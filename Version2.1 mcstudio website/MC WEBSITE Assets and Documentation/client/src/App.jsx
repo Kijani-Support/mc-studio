d2622aafdf6fa92072c9d2d4265c5f6be1f60af2
PeterMaks <peteryoungren57@gmail.com>
2026-04-07 10:52:37 +0300

    refine import statements

--- Version2.1 mcstudio website/MC WEBSITE Assets and Documentation/client/src/App.jsx
+++ Version2.1 mcstudio website/MC WEBSITE Assets and Documentation/client/src/App.jsx
@@ -1,14 +1,14 @@
-import './App.css';
+import '@/App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from './components/Context/ThemeContext'; // <-- Import the ThemeProvider
+import { ThemeProvider } from '@/components/Context/ThemeContext'; // <-- Import the ThemeProvider
 
-import HomePage from './pages/HomePage';
-import StartUpDirectoryPage from './pages/StartUpDirectoryPage';
-import StartUpProfilePage from './pages/StartUpProfilePage';
-import ProjectsPage from './pages/ProjectsPage';
-import CaseStudies from './pages/CaseStudies';
-import ArticleDetail from './pages/ArticleDetail';
-import Services from './pages/Services';
+import HomePage from '@/pages/HomePage';
+import StartUpDirectoryPage from '@/pages/StartUpDirectoryPage';
+import StartUpProfilePage from '@/pages/StartUpProfilePage';
+import ProjectsPage from '@/pages/ProjectsPage';
+import CaseStudies from '@/pages/CaseStudies';
+import ArticleDetail from '@/pages/ArticleDetail';
+import Services from '@/pages/Services';
 
 function App() {
   return (
